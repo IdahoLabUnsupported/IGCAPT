87b0f410a171623d54ac0182c5c93bde46cda1c3
Joe Frazier <joe.frazier@inl.gov>
2022-07-12 08:14:09 -0600

    Working on UI setup organization.

---
+++ src/main/java/org/openstreetmap/gui/jmapviewer/AddFieldMenuItem.java
@@ -0,0 +1,19 @@
+package org.openstreetmap.gui.jmapviewer;
+
+import javax.swing.*;
+
+public class AddFieldMenuItem extends JMenuItem {
+    private final FieldPanel panel;
+
+    AddFieldMenuItem() {
+        super("Add new Field");
+        panel = new FieldPanel();
+        createFieldMenu();
+    }
+
+    private void createFieldMenu() {
+        this.addActionListener(ActionListener -> {
+            panel.showConfirmDialog();
+        });
+    }
+}
