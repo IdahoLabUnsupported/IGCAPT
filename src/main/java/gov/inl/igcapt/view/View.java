d18cb8d54edda35e730bc22c61756f840e003124
dafew <douglas.few@gmail.com>
2023-01-19 12:14:14 -0700

    adding view.java

---
+++ src/main/java/gov/inl/igcapt/view/View.java
@@ -0,0 +1,238 @@
+package gov.inl.igcapt.view;
+
+import edu.uci.ics.jung.algorithms.layout.AbstractLayout;
+import edu.uci.ics.jung.algorithms.layout.GraphElementAccessor;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.algorithms.layout.StaticLayout;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.util.Context;
+import edu.uci.ics.jung.graph.util.Pair;
+import edu.uci.ics.jung.visualization.LayeredIcon;
+import edu.uci.ics.jung.visualization.MultiLayerTransformer;
+import edu.uci.ics.jung.visualization.RenderContext;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.GraphMouseListener;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse.Mode;
+import edu.uci.ics.jung.visualization.control.TranslatingGraphMousePlugin;
+import edu.uci.ics.jung.visualization.decorators.DefaultVertexIconTransformer;
+import edu.uci.ics.jung.visualization.picking.PickedState;
+import edu.uci.ics.jung.visualization.renderers.BasicEdgeRenderer;
+import edu.uci.ics.jung.visualization.renderers.Checkmark;
+import edu.uci.ics.jung.visualization.subLayout.GraphCollapser;
+import edu.uci.ics.jung.visualization.transform.shape.GraphicsDecorator;
+import gov.inl.igcapt.components.AggregationDialog;
+import gov.inl.igcapt.components.DependentUseCaseEntry;
+import gov.inl.igcapt.components.HelpDialog;
+import gov.inl.igcapt.components.NodeSettingsDialog;
+import gov.inl.igcapt.components.Payload;
+import gov.inl.igcapt.components.PayloadEditorForm;
+import gov.inl.igcapt.components.SgLayeredIcon;
+import gov.inl.igcapt.components.SgMapImage;
+import gov.inl.igcapt.components.UseCaseEntry;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Paint;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.Stroke;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.UUID;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import gov.inl.igcapt.components.DataModels.ComponentDao;
+import gov.inl.igcapt.components.DataModels.SgComponentData;
+import gov.inl.igcapt.components.DataModels.SgComponentGroupData;
+import gov.inl.igcapt.components.Heatmap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Collections;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+
+import gov.inl.igcapt.properties.IGCAPTproperties;
+import org.apache.commons.collections15.Factory;
+import org.apache.commons.collections15.Predicate;
+import org.apache.commons.collections15.Transformer;
+import org.openstreetmap.gui.jmapviewer.IGCAPTgui;
+import org.openstreetmap.gui.jmapviewer.JSGMapViewer;
+import org.openstreetmap.gui.jmapviewer.events.JMVCommandEvent;
+import org.openstreetmap.gui.jmapviewer.interfaces.ICoordinate;
+import org.openstreetmap.gui.jmapviewer.interfaces.JMapViewerEventListener;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileLoader;
+import org.openstreetmap.gui.jmapviewer.interfaces.TileSource;
+import org.openstreetmap.gui.jmapviewer.tilesources.BingAerialTileSource;
+import org.openstreetmap.gui.jmapviewer.tilesources.OsmTileSource;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+public class View extends JFrame implements JMapViewerEventListener, DropTargetListener {
+
+    ///Private Variables
+    private static View _view = new View();
+    private final JSGMapViewer treeMap = new JSGMapViewer("Component");
+    public View() {
+        super("Intelligent Grid Communications & Analysis Planning Tool");
+        this.initialize();
+    }
+
+    public int initialize(){
+        setSize(400, 400);
+        // Listen to the map viewer for user operations so components will
+        // receive events and update
+        treeMap.getViewer().addJMVListener(this);
+
+        setLayout(new BorderLayout());
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            @Override
+            public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+                confirmExit();
+            }
+        });
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        JPanel panel = new JPanel(new BorderLayout());
+        JPanel panelTop = new JPanel();
+        JPanel panelBottom = new JPanel();
+        JPanel helpPanel = new JPanel();
+
+
+        return(1);
+    }
+
+    private void confirmExit() {
+        if (false ){//fileDirty) {
+            if (JOptionPane.showConfirmDialog(IGCAPTgui.getInstance(),
+                    "Are you sure you want to exit? The current scenario has not been saved.", "Confirm Close",
+                    JOptionPane.YES_NO_OPTION,
+                    JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION) {
+
+                IGCAPTproperties.getInstance().storeProperties();
+                System.exit(0);
+            }
+        } else {
+            IGCAPTproperties.getInstance().storeProperties();
+            System.exit(0);
+        }
+    }
+
+    /**
+     * @param dtde the {@code DropTargetDragEvent}
+     */
+    @Override
+    public void dragEnter(DropTargetDragEvent dtde) {
+
+    }
+
+    /**
+     * @param dtde the {@code DropTargetDragEvent}
+     */
+    @Override
+    public void dragOver(DropTargetDragEvent dtde) {
+
+    }
+
+    /**
+     * @param dtde the {@code DropTargetDragEvent}
+     */
+    @Override
+    public void dropActionChanged(DropTargetDragEvent dtde) {
+
+    }
+
+    /**
+     * @param dte the {@code DropTargetEvent}
+     */
+    @Override
+    public void dragExit(DropTargetEvent dte) {
+
+    }
+
+    /**
+     * @param dtde the {@code DropTargetDropEvent}
+     */
+    @Override
+    public void drop(DropTargetDropEvent dtde) {
+
+    }
+
+    /**
+     * @param command
+     */
+    @Override
+    public void processCommand(JMVCommandEvent command) {
+
+    }
+}
