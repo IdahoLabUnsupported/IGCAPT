a9a95233b48fcdf019da3166e217c16853cb6d23
chedoom3 <cherie.phelan@inl.gov>
2023-04-26 08:19:04 -0600

    Add threading to the wizard.

--- src/main/java/gov/inl/igcapt/view/IGCAPTgui.java
+++ src/main/java/gov/inl/igcapt/view/IGCAPTgui.java
@@ -876,7 +876,7 @@ public class IGCAPTgui extends JFrame implements JMapViewerEventListener, DropTa
         });
         
         newScenario.addActionListener((ActionEvent ev) -> {
-            CreateScenarioWizard createScenarioWizard = new CreateScenarioWizard(this, true);
+            new CreateScenarioWizard(this, true);
         });
         
         initConnection.addActionListener((ActionEvent ev) -> {
